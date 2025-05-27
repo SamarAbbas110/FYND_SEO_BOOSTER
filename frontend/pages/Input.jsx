5cff87f7116c9cc01683010df8a46643514aefed
Samar Abbas <71007148+SamarAbbas110@users.noreply.github.com>
2025-05-27 12:16:22 +0530

    Removed Unnecessary Codes

---
+++ frontend/pages/Input.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style/Input.css";
+
+const Input = () => {
+  return (
+    <div>
+      <div>
+        <h1>Hi, This is your SEO Booster</h1>
+      </div>
+      <div>
+
+      <input class="home-input" type="text" />
+      </div>
+    </div>
+  );
+};
+
+export default Input;
